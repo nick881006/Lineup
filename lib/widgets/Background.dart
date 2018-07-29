57f830390a42d42c0705ad6406d7fc6314c016ee
nick lee <nicklee1006@gmail.com>
2018-07-29 23:28:06 +0930

    create Background Widget

---
+++ lib/widgets/Background.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BackgroundWidget extends StatelessWidget {
+  final String imagePath;
+
+  BackgroundWidget({Key key, this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: new BoxDecoration(
+          image: new DecorationImage(
+              image: new AssetImage(imagePath),
+              fit: BoxFit.cover
+          )
+      ),
+    );
+  }
+}
