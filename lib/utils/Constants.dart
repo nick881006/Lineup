444774a7fd43b833eec7573db7366810366ddff6
nick lee <nicklee1006@gmail.com>
2018-08-07 21:41:42 +0930

    add role string constants

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -2,4 +2,7 @@ class Constants {
   static final double widgetSize = 40.0;
   static final double fieldTBRatio = 0.16;
   static final double fieldLRRatio = 0.04;
+  static final String manager = 'manager';
+  static final String main = 'main';
+  static final String sub = 'sub';
 }
